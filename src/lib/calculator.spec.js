a87799ec01881a4cd8baf97f77e6f13eb45d4c8b
Leo Silva <leocardosodev@gmail.com>
2023-09-15 16:13:51 -0300

    feat: add calculator sum completed

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,6 +1,29 @@
 const { sum } = require('./calculator');
 describe("Calculator test", () => {
+
   it("Should sum 2 and 2 and the result must be 4", () => {
     expect(sum(2,2)).toBe(4);
+  });
+
+  it("Should sum 2 and 2 even if one of them ias a string and the result must be 5", () => {
+    expect(sum("2","2")).toBe(4);
+  });
+
+  it("Should throw an error if what is provided to the method cannot be sum", () => {
+    expect(() => {
+      sum("","2");
+    }).toThrowError('Please check your input');
+    
+    expect(() => {
+      sum([2,2]);
+    }).toThrowError('Please check your input');
+
+    expect(() => {
+      sum({});
+    }).toThrowError('Please check your input');
+
+    expect(() => {
+      sum();
+    }).toThrowError('Please check your input');
   });
 });
