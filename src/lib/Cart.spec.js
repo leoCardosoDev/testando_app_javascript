1e6fe15de431f5ee109dc8552dd75a4da279c667
leoCardosoDev <leocardosodev@gmail.com>
2023-11-16 08:39:35 -0300

    test: add getTotal returns total if products and quantity is provided

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -1,9 +1,26 @@
 import Cart from './Cart'
 
 describe('Cart', () => {
+  let cart
+
+  beforeEach(() => {
+    cart = new Cart
+  })
+
   it('Should return 0 when getTotal() is executed in a newly created instance', () => {
-    const cart = new Cart()
-    expect(cart.getTotal()).toEqual(0)
+        expect(cart.getTotal()).toEqual(0)
+  })
+
+  it('Should multiply quantity and price and receive the total amount ', () => {
+    const item = {
+      product: {
+        title: 'Adidas running shoes - men',
+        price: 35388 // 353.88 | R$ 353,88
+      },
+      quantity: 2 // 70776
+    }
+    cart.add(item)
+    expect(cart.getTotal()).toEqual(70776)
   })
 })
 
