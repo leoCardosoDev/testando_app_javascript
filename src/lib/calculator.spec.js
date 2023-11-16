4a3070b20d76889d64eb6cee31a32801b1d8fc03
leoCardosoDev <leocardosodev@gmail.com>
2023-11-16 06:29:38 -0300

    refactor: change prettier

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,29 +1,28 @@
 const { sum } = require('./calculator');
-describe("Calculator test", () => {
+describe('Calculator test', () => {
+  it('Should sum 2 and 2 and the result must be 4', () => {
+    expect(sum(2, 2)).toBe(4);
+  });
 
-  it("Should sum 2 and 2 and the result must be 4", () => {
-    expect(sum(2,2)).toBe(4)
-  })
+  it('Should sum 2 and 2 even if one of them ias a string and the result must be 5', () => {
+    expect(sum('2', '2')).toBe(4);
+  });
 
-  it("Should sum 2 and 2 even if one of them ias a string and the result must be 5", () => {
-    expect(sum("2","2")).toBe(4)
-  })
-
-  it("Should throw an error if what is provided to the method cannot be sum", () => {
+  it('Should throw an error if what is provided to the method cannot be sum', () => {
     expect(() => {
-      sum("","2")
-    }).toThrow('Please check your input')
-    
-    expect(() => {
-      sum([2,2])
-    }).toThrow('Please check your input')
+      sum('', '2');
+    }).toThrow('Please check your input');
 
     expect(() => {
-      sum({})
-    }).toThrow('Please check your input')
+      sum([2, 2]);
+    }).toThrow('Please check your input');
 
     expect(() => {
-      sum()
-    }).toThrow('Please check your input')
+      sum({});
+    }).toThrow('Please check your input');
+
+    expect(() => {
+      sum();
+    }).toThrow('Please check your input');
   });
 });
--- src/lib/query_string.js
+++ src/lib/query_string.js
@@ -1,12 +1,12 @@
 const keyValueToString = ([key, val]) => {
   if (typeof val === 'object' && !Array.isArray(val)) {
-    throw new Error('Please check you params')
+    throw new Error('Please check you params');
   }
-  return `${key}=${val}`
-}
+  return `${key}=${val}`;
+};
 
 module.exports.queryString = obj =>
-  Object.entries(obj).map(keyValueToString).join('&')
+  Object.entries(obj).map(keyValueToString).join('&');
 
 module.exports.parse = string =>
-  Object.fromEntries(string.split('&').map(item => item.split('=')))
+  Object.fromEntries(string.split('&').map(item => item.split('=')));
