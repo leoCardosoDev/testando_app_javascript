04c0c5eb24f6678bc6141cd93d95dc9649fa6006
leoCardosoDev <leocardosodev@gmail.com>
2023-11-16 06:42:25 -0300

    refactor: change prettie to remove semicolon

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,28 +1,28 @@
-const { sum } = require('./calculator');
+const { sum } = require('./calculator')
 describe('Calculator test', () => {
   it('Should sum 2 and 2 and the result must be 4', () => {
-    expect(sum(2, 2)).toBe(4);
-  });
+    expect(sum(2, 2)).toBe(4)
+  })
 
   it('Should sum 2 and 2 even if one of them ias a string and the result must be 5', () => {
-    expect(sum('2', '2')).toBe(4);
-  });
+    expect(sum('2', '2')).toBe(4)
+  })
 
   it('Should throw an error if what is provided to the method cannot be sum', () => {
     expect(() => {
-      sum('', '2');
-    }).toThrow('Please check your input');
+      sum('', '2')
+    }).toThrow('Please check your input')
 
     expect(() => {
-      sum([2, 2]);
-    }).toThrow('Please check your input');
+      sum([2, 2])
+    }).toThrow('Please check your input')
 
     expect(() => {
-      sum({});
-    }).toThrow('Please check your input');
+      sum({})
+    }).toThrow('Please check your input')
 
     expect(() => {
-      sum();
-    }).toThrow('Please check your input');
-  });
-});
+      sum()
+    }).toThrow('Please check your input')
+  })
+})
