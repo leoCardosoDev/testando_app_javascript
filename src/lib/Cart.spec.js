058ed91494fc8c3168038a65e89f8ac15ab210f1
leoCardosoDev <leocardosodev@gmail.com>
2023-11-16 09:18:30 -0300

    test: remove items if item is replicated

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -2,9 +2,14 @@ import Cart from './Cart'
 
 describe('Cart', () => {
   let cart
+  let product
 
   beforeEach(() => {
     cart = new Cart
+    product = {
+      title: 'Adidas running shoes - men',
+      price: 35388 // 353.88 | R$ 353,88
+    }
   })
 
   it('Should return 0 when getTotal() is executed in a newly created instance', () => {
@@ -13,14 +18,24 @@ describe('Cart', () => {
 
   it('Should multiply quantity and price and receive the total amount ', () => {
     const item = {
-      product: {
-        title: 'Adidas running shoes - men',
-        price: 35388 // 353.88 | R$ 353,88
-      },
+      product,
       quantity: 2 // 70776
     }
     cart.add(item)
     expect(cart.getTotal()).toEqual(70776)
+  })
+
+  it("Should ensure no more than on product exists at a time", () => {
+    cart.add({
+      product,
+      quantity: 2
+    })
+    cart.add({
+      product,
+      quantity: 1
+    })
+
+    expect(cart.getTotal()).toEqual(35388)
   })
 })
 
