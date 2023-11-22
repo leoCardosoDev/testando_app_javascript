b114b9aa14fcc71d4617d3ba0edf462e11377db2
leoCardosoDev <leocardosodev@gmail.com>
2023-11-22 13:47:49 -0300

    feat: add Cart.spec.js

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -2,14 +2,18 @@ import Cart from './Cart'
 
 describe('Cart', () => {
   let cart
-  let product
+  let product = {
+    title: 'Adidas running shoes - men',
+    price: 35388 // 353.88 | R$ 353,88
+  }
+
+  let product2 = {
+    title: 'Adidas running shoes - women',
+    price: 41872 // 353.88 | R$ 353,88
+  }
 
   beforeEach(() => {
     cart = new Cart
-    product = {
-      title: 'Adidas running shoes - men',
-      price: 35388 // 353.88 | R$ 353,88
-    }
   })
 
   it('Should return 0 when getTotal() is executed in a newly created instance', () => {
@@ -36,6 +40,17 @@ describe('Cart', () => {
     })
 
     expect(cart.getTotal()).toEqual(35388)
+  })
+
+  it('Should update total when a product gets included and then removed', () => {
+    cart.add({
+      product,
+      quantity: 2
+    })
+    cart.add({
+      product: product2,
+      quantity: 2
+    })
   })
 })
 
