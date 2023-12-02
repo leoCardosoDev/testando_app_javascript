07210f0b91cf5852aca1ef63660f63007ae4c243
leoCardosoDev <leocardosodev@gmail.com>
2023-12-02 06:06:45 -0300

    feat: add formatted currency

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -75,16 +75,18 @@ export default class Cart {
     const { total, items } = this.summary()
     this.items = []
     return {
-      total,
+      total: total.getAmount(),
       items,
     }
   }
 
   summary() {
-    const total = this.getTotal().getAmount()
+    const total = this.getTotal()
+    const formatted = total.toFormat('$0,0.00')
     const items = this.items
     return {
       total,
+      formatted,
       items,
     }
   }
